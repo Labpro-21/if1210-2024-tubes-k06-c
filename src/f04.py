7e88a3f823935844417ca4527f45a2c01301b053
SaccaKovidaKasmaji <16523146@std.stei.itb.ac.id>
2024-05-01 13:19:19 +0700

    style: add spacing and print message

--- src/f04.py
+++ src/f04.py
@@ -10,20 +10,12 @@ id = user[username][id]
 
 # Kode di sini
 def help(id):
-    if userid == 0:
-        print("=========== HELP ===========")
-        print("Kamu belum login sebagai role apapun. Silahkan login terlebih dahulu.")
-        print("   1. Login: Masuk ke dalam akun yang sudah terdaftar.")
-        print("   2. Register: Membuat akun baru.")
-        print("                                                                               ")
-        print("Footnote:")
-        print("   1. Untuk menggunakan aplikasi, silahkan masukkan nama fungsi yang terdaftar.")
-        print("   2. Jangan lupa untuk memasukkan input yang valid.")
-    else: # id != 0 
-
+    if id True
         if role == "admin":
             print("=========== HELP ===========")
+            print("                                                                               ")
             print("Selamat datang, Admin. Berikut adalah hal-hal yang dapat kamu lakukan.")
+            print("                                                                               ")
             print("   1. Logout            : Keluar dari akun yang sedang digunakan.")
             print("   2. Shop              : Melakukan manajemen pada SHOP sebagai tempat jual beli peralatan Agent.")
             print("   3. Monster Management: Menambahkan monster baru ke dalam database.") 
@@ -31,10 +23,12 @@ def help(id):
             print("Footnote:")
             print("   1. Untuk menggunakan aplikasi, silahkan masukkan nama fungsi yang terdaftar.")
             print("   2. Jangan lupa untuk memasukkan input yang valid.")
-        elif role == agent :
-            if role == agnet   then:
+        else: #  role == agent 
             print("=========== HELP ===========")
-            print(f"Selamat datang {username}. Berikut adalah hal-hal yang dapat kamu lakukan.")
+            print("                                                                               ")
+            print(f"Halo Agent {username}.Kamu memanggil command HELP. Kamu memilih jalan yang benar, semoga kamu ")
+            print("tidak sesat kemudian. Berikut adalah hal-hal yang dapat kamu lakukan sekarang: ")
+            print("                                                                               ")
             print("   1. Logout    : Keluar dari akun yang sedang digunakan.")
             print("   2. Shop      : Membeli agent, monster dan potion.")
             print("   3. Inventory : Menampilkan monster dan potion serta spesifikasi yang dimiliki .")    
@@ -44,10 +38,19 @@ def help(id):
             print("                                                                               ")                                                                                                                                                ")
             print("Footnote:")
             print("   1. Untuk menggunakan aplikasi, silahkan masukkan nama fungsi yang terdaftar.")
-            print("   2. Jangan lupa untuk memasukkan input yang valid.")
-        else: 
-            print("ok")
-        
+            print("   2. Jangan lupa untuk memasukkan input yang valid.")  
+    else: # id != 0 
+        print("=========== HELP ===========")
+        print("                                                                               ")
+        print("Kamu belum login sebagai role apapun. Silahkan login terlebih dahulu.")
+        print("                                                                               ")
+        print("   1. Login: Masuk ke dalam akun yang sudah terdaftar.")
+        print("   2. Register: Membuat akun baru.")
+        print("                                                                               ")
+        print("Footnote:")
+        print("   1. Untuk menggunakan aplikasi, silahkan masukkan nama fungsi yang terdaftar.")
+        print("   2. Jangan lupa untuk memasukkan input yang valid.")
+  
 
 """""
 DESKRIPSI
