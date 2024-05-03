2b03560d6f32e4edbe69bf03c1d56856f88a7712
NayakaZNA <19623116@std.stei.itb.ac.id>
2024-05-04 00:31:09 +0700

    refactor: change data structure for csv_parser output to resemble Pandas' datarame

--- src/x02.py
+++ src/x02.py
@@ -2,29 +2,47 @@
 X01 - Fungsi-fungsi Pembantu 2
 19623116 Nayaka
 """
+import os, sys
+# Mengimpor fungsi-fungsi yang sudah dibuat dengan penuh jerih payah
+src_path = os.path.join(os.path.dirname(__file__), 'src')
+data_path = os.path.join(os.path.dirname(__file__), 'data')
+sys.path.append(src_path)
+sys.path.append(data_path)
+
 from x01 import *
 # Jika ingin menjalankan file ini secara independen
 # (bukan sebagai module dari main.py) hilangkan "src."
 
 def read_header(path: str) -> str:
-    file =  open(path, 'r')
+    file = open(path, 'r')
     for line in file:
         return line.strip()
 
-def csv_parser(path: str) -> list[dict]:
-    data = []
+def csv_parser(path: str, identifier: str) -> list[dict]:
     file = open(path, 'r+')
     headers = split_str(read_header(path))
+    data = {header : {} for header in headers}
+    
+    identifier_idx = -1
+    for i in range(len(headers)):
+        if headers[i] == identifier:
+            identifier_idx = i
+    if identifier_idx == -1:
+        raise ValueError("Identifier tidak ditemukan!")
+
+    # Menambahkan data pada tiap kolom satu per satu
     for line in file:
         entries = split_str(strip_str(line))
         if len(entries) != len(headers):
             raise ValueError("Ada data yang kosong atau melebihi kolom header!")
         if entries != headers:
-            row_dict = {}
+            identifier_key = entries[identifier_idx]
             for i in range(len(headers)):
-                row_dict[headers[i]] = entries[i]
-            data.append(row_dict)
+                header = headers[i]
+                data[header][identifier_key] = entries[i]
     return data
+
+print(csv_parser("data/user.csv", "username"))
 
 def get_val(db: list[dict], val, src, tget) -> int:
     for i in range(len(db)):
