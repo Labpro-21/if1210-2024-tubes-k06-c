5e6eab31e3ebba51206238ca0cb399357c79c17f
NayakaZNA <19623116@std.stei.itb.ac.id>
2024-05-06 05:27:44 +0700

    refactor: add input_username() and input_password() functions

--- src/f01.py
+++ src/f01.py
@@ -3,14 +3,56 @@ F01 - Register
 19623116 Nayaka
 """
 
+# Membuka path ke folder 'data'
+import sys, os
+data_path = os.path.join(os.path.dirname(__file__), 'data')
+sys.path.append(data_path)
+
+# Memuat file .csv yang diperlukan
+from x02 import *
+user_db = csv_parser("data/user.csv", "username")
+monster_db = csv_parser("data/monster.csv", "type")
+
+# FUNGSI input_username()
+def input_username():
+    global user_db
+    valid_char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
+    username = input("Username: ")
+
+    valid = all_is_in(username, valid_char)
+    while not valid:
+        remove_nth_line(1)
+        print("Username hanya boleh berisi alfabet, angka, tanda hubung bawah (_), dan tanda strip (-)!")
+        username = input("Username: ")
+        remove_nth_line(1)
+        valid = all_is_in(username, valid_char)
+        if valid:
+            remove_nth_line(1)
+    return username
+
+# FUNGSI input_password()
+def input_password(username: str):
+    global user_db
+    password = input("Password: ")
+    if user_db["password"][username] == password:
+            return password
+    else:
+        while user_db["password"][username] != password:
+            print("negro!")
+            password = input("Password: ")
+
+# FUNGSI register()
 def register():
+
+    # Ra'if ini ntar tolong dicakepin yah itunya
     print("<=============>")
     print("  O. W. C. A.  ")
     print("   We serve.   ")
     print("<=============>")
 
-    username = input("Username: ")
-    password = input("Password: ")
+    # Bagian utama fungsi
+    username = input_username()
+    password = input_password(username)
     
     
 register()
