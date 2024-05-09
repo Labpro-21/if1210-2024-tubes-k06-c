ee9a10a13755c76d1c94be1184ceb4076db257d0
NayakaZNA <darkelytra@gmail.com>
2024-05-10 05:40:51 +0700

    refactor: move x02 (deprecated) functions to f14; style: rename csv_parser() to load()

--- src/f14.py
+++ src/f14.py
@@ -1,9 +1,38 @@
 """
 F14 - Load
-NIM NAMA
+19623116 Nayaka
 """
+import os, sys
+src_path = os.path.join(os.path.dirname(__file__), 'src')
+sys.path.append(src_path)
+data_path = os.path.join(os.path.dirname(__file__), 'data')
+sys.path.append(data_path)
 
-# Kode di sini
+from src.x01 import *
+
+def read_header(path: str) -> str:
+    file = open(path, 'r')
+    for line in file:
+        return line.strip()
+
+def load(path: str) -> dict:
+    file = open(path, 'r')
+    headers = split_str(read_header(path))
+    data = {header : [] for header in headers}
+
+    # Menambahkan data pada tiap kolom satu per satu
+    for line in file:
+        entries = split_str(strip_str(line))
+        # Error ketika ada panjang baris yang tidak sama dengan header
+        if len(entries) != len(headers):
+            raise ValueError("Ada data yang kosong atau melebihi kolom header!")
+        # Memproses data csv untuk baris-baris setelah header
+        ## Mengambil identitas dari identifier yang ditentukan
+        if entries != headers:
+            for i in range(len(headers)):
+                header, entry = headers[i], entries[i]
+                data[header].append(entry)
+    return data
 
 """
 DESKRIPSI
