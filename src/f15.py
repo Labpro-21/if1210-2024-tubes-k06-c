5b1ce253b905458b249771b01fedff06f09c7760
NayakaZNA <darkelytra@gmail.com>
2024-05-10 05:42:21 +0700

    feat: add save() function

--- src/f15.py
+++ src/f15.py
@@ -1,9 +1,36 @@
 """
 F15 - Save
-NIM NAMA
+19623116 Nayaka
 """
 
-# Kode di sini
+def save(db: dict, path: str):
+    csv = open(path, "w")
+    db_width = len(db)
+    headers = []
+    headers_row = ""
+
+    i = 0
+    for header in db.keys():
+        headers.append(header)
+
+        if i != db_width - 1:
+            headers_row = f"{headers_row}{header};"
+        else:
+            headers_row = f"{headers_row}{header}"
+
+        i += 1
+
+    db_length = len(db[headers[0]])
+    for i in range(db_length):
+        row = ""
+        for j in range(db_width):
+            if j != db_width - 1:
+                row = f"{row}{db[headers[j]][i]};"
+            else:
+                row = f"{row}{db[headers[j]][i]}"
+        csv.write(row)
+        csv.write("\n")
+
 
 """
 DESKRIPSI
