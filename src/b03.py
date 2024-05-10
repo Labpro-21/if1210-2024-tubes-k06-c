bf0921902e4dc7eeaf57be7c9b604d20a8738aff
Muhammad Ra'if Alkautsar <96478279+lotsadelight@users.noreply.github.com>
2024-05-10 20:25:38 +0700

    feat: adds monster ball procedure.

--- src/b03.py
+++ src/b03.py
@@ -2,8 +2,46 @@
 FB03 - Monster Ball
 NIM NAMA
 """
+from f00 import *
 
-# Kode di sini
+def monsterball():
+    global enemyMonster
+    result = rng(0, 20, time.time())
+    if enemyMonster["lv"] == 1:
+        if 0 < result < 15:
+            print("Selamat, monster berhasil tertangkap!")
+            monsterCaught()
+            isBattle = False
+        else: 
+            print("Monster lepas!")
+    elif enemyMonster["lv"] == 2:
+        if 0 < result < 10:
+            print("Monster berhasil tertangkap!")
+            monsterCaught()
+            isBattle = False
+        else:
+            print("Monster lepas!")
+    elif enemyMonster["lv"] == 3:
+        if 0 < result < 5:
+            print("Monster berhasil tertangkap!")
+            monsterCaught()
+            isBattle = False
+        else:
+            print("Monster lepas!")
+    elif enemyMonster["lv"] == 4:
+        if 0 < result < 2:
+            print("Monster berhasil tertangkap!")
+            monsterCaught()
+            isBattle = False
+        else:
+            print("Monster lepas!")
+    else:
+        if 0 < result < 1:
+            print()
+            monsterCaught()
+            isBattle = False
+        else:
+            print("Monster lepas!")
 
 """
 DESKRIPSI
