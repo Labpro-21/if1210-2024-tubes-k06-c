a0b149f42cc06d86895b24d7385439004d244332
NayakaZNA <darkelytra@gmail.com>
2024-05-11 13:06:54 +0700

    docs: add function description

--- src/f01.py
+++ src/f01.py
@@ -6,22 +6,6 @@ F01 - Register
 # Membuka database yang sudah di-load dan disimpan di global_var
 from global_var import *
 
-# FUNGSI input_username()
-def input_username_reg():
-    valid_char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
-    username = input("Username: ")
-
-    valid = subset(username, valid_char)
-    while not valid:
-        remove_nth_line(1)
-        print("Username hanya boleh berisi alfabet, angka, tanda hubung bawah (_), dan tanda strip (-)!")
-        username = input("Username: ")
-        remove_nth_line(1)
-        valid = subset(username, valid_char)
-        if valid:
-            remove_nth_line(1)
-    return username
-
 # FUNGSI register()
 def register():
     # Ra'if ini ntar tolong dicakepin yah itunya
@@ -40,9 +24,30 @@ def register():
     user_db["role"].append('agent')
     user_db["oc"].append(0)
 
+# FUNGSI input_username_reg()
+def input_username_reg():
+    valid_char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
+    username = input("Username: ")
+
+    valid = subset(username, valid_char)
+    while not valid:
+        remove_nth_line(1)
+        print("Username hanya boleh berisi alfabet, angka, tanda hubung bawah (_), dan tanda strip (-)!")
+        username = input("Username: ")
+        remove_nth_line(1)
+        valid = subset(username, valid_char)
+        if valid:
+            remove_nth_line(1)
+    return username
+
 """
 DESKRIPSI
 Penjelasan ini ditaruh sementara dan akan dihapus pada rilis versi final.
+Fungsi register() akan mendaftarkan akun baru untuk agent dengan username dan password
+sesuai dengan yang diinput oleh pengguna. Fungsi input_username_reg() digunakan untuk
+menerima username dari akun yang akan dibuat dan memvalidasinya. 
 
-
+Di sini global_var dimuat agar kita dapat mengakses database username dari user.csv yang sudah di-load.
+Perhatikan bahwa remove_nth_line() yang merupakan fungsi dari x01 dapat digunakan tanpa
+memuat x01.py secara eksplisit karena sudah dimuat lewat f14 dari global_var.
 """
