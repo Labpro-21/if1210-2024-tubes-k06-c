87a1affdc255b6133c22226ebe60728316382674
Muhammad Ra'if Alkautsar <96478279+lotsadelight@users.noreply.github.com>
2024-05-13 10:35:02 +0700

    refactor: turns monsterball procedure into a function

--- src/b03.py
+++ src/b03.py
@@ -1,47 +1,56 @@
 """
 FB03 - Monster Ball
-NIM NAMA
+19623296 / Muhammad Ra'if Alkautsar
 """
 from f00 import *
+from global_var import *
 
-def monsterball():
-    global enemyMonster
+def monsterball(level: int) -> bool:
     result = rng(0, 20, time.time())
-    if enemyMonster["lv"] == 1:
+    if level == 1:
         if 0 < result < 15:
-            print("Selamat, monster berhasil tertangkap!")
-            monsterCaught()
-            isBattle = False
+            return(True)
         else: 
-            print("Monster lepas!")
-    elif enemyMonster["lv"] == 2:
+            return(False)
+    elif level == 2:
         if 0 < result < 10:
-            print("Monster berhasil tertangkap!")
-            monsterCaught()
-            isBattle = False
+            return(True)
         else:
-            print("Monster lepas!")
-    elif enemyMonster["lv"] == 3:
+            return(False)
+    elif level == 3:
         if 0 < result < 5:
-            print("Monster berhasil tertangkap!")
-            monsterCaught()
-            isBattle = False
+            return(True)
         else:
-            print("Monster lepas!")
-    elif enemyMonster["lv"] == 4:
+            return(False)
+    elif level == 4:
         if 0 < result < 2:
-            print("Monster berhasil tertangkap!")
-            monsterCaught()
-            isBattle = False
+            return(True)
         else:
-            print("Monster lepas!")
+            return(False)
     else:
         if 0 < result < 1:
-            print()
-            monsterCaught()
-            isBattle = False
+            return(True)
         else:
-            print("Monster lepas!")
+            return(False)
+
+""" def monster_caught(): # KODE RUSAK
+    i = 0
+    temp_db = {"user_id":[], "monster_id":[], "level":[]}
+    while temp_db["user_id"][i] <= user:
+        temp_db["user_id"].append(monster_inv_db["user_id"][i])
+        temp_db["monster_id"].append(monster_inv_db["monster_id"][i])
+        temp_db["level"].append(monster_inv_db["level"][i])
+        i += 1
+    temp_db["user_id"].append(monster_inv_db["user_id"][i])
+    temp_db["monster_id"].append(monster_inv_db["monster_id"][i])
+    temp_db["level"].append(monster_inv_db["level"][i])
+    i += 1 
+    while i <= (len(monster_inv_db["user_id"]) + 1):
+        temp_db["user_id"].append(user)
+        temp_db["monster_id"].append(random_index + 1)
+        temp_db["level"].append(enemy_level)
+        i += 1
+    monster_inv_db = temp_db """
 
 """
 DESKRIPSI
