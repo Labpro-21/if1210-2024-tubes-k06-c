94a31f229edcb0eabbcd33be7b5d544293ad5469
NayakaZNA <darkelytra@gmail.com>
2024-05-18 22:19:04 +0700

    fix: remove import of global_var

--- src/F03_Logout.py
+++ src/F03_Logout.py
@@ -3,16 +3,10 @@ F03 - Logout
 19623116 Nayaka
 """
 
-from src.global_var import *
-
 def logout(logged_in: bool):
     if logged_in:
         print("Berhasil logout!")
-<<<<<<< Updated upstream
         return 1
-=======
-        logged_in = False
->>>>>>> Stashed changes
     else: # not logged_in
         print("Logout gagal karena Anda belum login!")
 
