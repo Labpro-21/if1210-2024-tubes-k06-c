9397a5545c6ec62fbf6f617ca0078f1da6f3dfda
NayakaZNA <darkelytra@gmail.com>
2024-05-18 15:18:47 +0700

    fix: initialize databases as empty dictionaries

--- global_var.py
+++ global_var.py
@@ -3,11 +3,11 @@ start = time.time()
 
 from src.F14_Load import *
 
-user_db = load('data/user.csv')
-monster_db = load('data/monster.csv')
-monster_shop_db = load('data/monster_shop.csv')
-monster_inv_db = load('data/monster_inventory.csv')
-item_shop_db = load('data/item_shop.csv')
-item_inv_db = load('data/item_inventory.csv')
+user_db = {}
+monster_db = {}
+monster_shop_db = {}
+monster_inv_db = {}
+item_shop_db = {}
+item_inv_db = {}
 
 logged_in = False
