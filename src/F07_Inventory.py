6d85ef9b8c94b829b6e11ade669717f49af75ab9
Muhammad Ra'if Alkautsar <96478279+lotsadelight@users.noreply.github.com>
2024-05-19 12:20:43 +0700

    fix: adds condition for users with no monster or no item

--- src/F07_Inventory.py
+++ src/F07_Inventory.py
@@ -14,25 +14,32 @@ def get_monster(i, lv, monster_db): # mengambil data monster
     return(f"{monster_to_get[0]} (ATK Power: {monster_to_get[1]} | DEF Power: {monster_to_get[2]} | HP: {monster_to_get[3]})")
 
 def show_items(item_inv_db, user_id):
+    found = False
     i = 0
     j = 1
     while i < len(item_inv_db['user_id']) and item_inv_db['user_id'][i] != user_id:
         i += 1
     while i < len(item_inv_db['user_id']) and item_inv_db['user_id'][i] == user_id:
+        found = True
         print(f"{j}. {(item_inv_db['type'][i]).title()} (Quantity: {item_inv_db['quantity'][i]})")
         i += 1
         j += 1
-
+    if not found:
+        print("Tidak mempunyai item!")
 
 def show_monsters(monster_inv_db, monster_db, user_id):
+    found = False
     i = 0
     j = 1
     while i < len(monster_inv_db['user_id']) and monster_inv_db['user_id'][i] != user_id:
         i += 1
     while i < len(monster_inv_db['user_id']) and monster_inv_db['user_id'][i] == user_id:
+        found = True
         print(f"{j}. {get_monster(monster_inv_db['monster_id'][i], monster_inv_db['level'][i], monster_db)}")
         i += 1
         j += 1
+    if not found:
+        print("Tidak mempunyai monster!")
 
 def get_start_index(db, user_id) -> int: # Fungsi ini dibuat untuk mendapatkan indeks pertama dari barang pengguna, digunakan pada monster inventory dan item inventory.
     i = 0
